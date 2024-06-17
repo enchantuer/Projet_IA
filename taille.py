705c6a597749eecb47dc0992d02addb30760b23d
Florent <florent.butery@isen-ouest.yncrea.fr>
2024-06-17 11:24:55 +0200

    ajout fonction todo

--- taille.py
+++ taille.py
@@ -6,11 +6,16 @@ def load_data(file_path):
     return pd.read_csv(file_path)
 
 def generate_model(data_path, k):
+    # TODO : Création du model
     model = KMeans(n_clusters=k)
     data = load_data(data_path)
     X = data.drop("haut_tot", axis=1).drop("haut_tronc", axis=1)
-    Y = data["haut_tot"].copy()
-
     kmeans = model.fit(X)
+
+    return kmeans
+
+def test_model(model, data_path):
+    # TODO : Metric du model
+    pass
 
 m = generate_model("Data_Arbre.csv")
