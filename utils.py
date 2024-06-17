28377133570ec228882b138030e49207f96a2c3f
Florent <florent.butery@isen-ouest.yncrea.fr>
2024-06-17 11:56:47 +0200

    remove todo

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 
 def load_data(file_path):
-    # TODO : Traitement des données
     data = pd.read_csv(file_path)
 
     liste_modif=["clc_quartier","clc_secteur","fk_arb_etat","fk_stadedev","fk_port","fk_pied","fk_situation","fk_revetement","fk_nomtech","villeca","feuillage","remarquable"]
