047c89020027345bce97371f4b270bb98f0cf931
pbroc <paul.brochoire@isen-ouest.yncrea.fr>
2024-06-17 11:49:26 +0200

    vectorisation data

--- utils.py
+++ utils.py
@@ -5,10 +5,20 @@ def load_data(file_path):
     # TODO : Traitement des données
     data = pd.read_csv(file_path)
 
+    liste_modif=["clc_quartier","clc_secteur","fk_arb_etat","fk_stadedev","fk_port","fk_pied","fk_situation","fk_revetement","fk_nomtech","villeca","feuillage","remarquable"]
+    for i in liste_modif:
+        modif_data(data,i)
+    return data
+
+
+def modif_data(data,column):
     temp = OrdinalEncoder()
-    train_cat = data[["fk_stadedev"]]
-    a = temp.fit_transform(train_cat)
-    # print(a)
-    data["fk_stadedev"] = a
+    train_cat = data[[column]]
+    data[column] = temp.fit_transform(train_cat)
 
     return data
+
+
+if __name__ == "__main__":
+    print(load_data("Data_Arbre.csv"))
+    pass
