49838edc691121f5899b9ffc0ba5b30c289cbb33
pbroc <paul.brochoire@isen-ouest.yncrea.fr>
2024-06-17 11:31:40 +0200

    s

--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
+
 def load_data(file_path):
     # TODO : Traitement des données
-    pass
+    data = pd.read_csv(file_path)
+
+    temp = OrdinalEncoder()
+    train_cat = data[["fk_stadedev"]]
+    a = temp.fit_transform(train_cat)
+    # print(a)
+    data["fk_stadedev"] = a
+
+    return data
