220e90515c8bdcc1cf92d90d845f9839f24e91db
pbroc <paul.brochoire@isen-ouest.yncrea.fr>
2024-06-18 14:52:31 +0200

    Métriques pour la classification

--- temp.py
+++ temp.py
@@ -8,15 +8,28 @@ import utils as ut
 import numpy as np
 from sklearn import model_selection
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.metrics import confusion_matrix
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 def load_data(file_path):
     # TODO : Choix des données à conservé
     data = ut.load_data(file_path)
     return data
+
+
+def create_classes(data):
+    # Définir les bornes et les étiquettes des classes
+    bins = [0, 20, 40, 60, np.inf]
+    labels = [0, 1, 2, 3]
+
+    # Créer une nouvelle colonne 'AgeClass' pour les classes d'âge
+    data["age_class"] = pd.cut(data["age_estim"], bins=bins, labels=labels, right=False)
+
+    return data
+
 
 def generate_model_age(data):
     # TODO : Création du model
@@ -26,40 +39,38 @@ def generate_model_age(data):
                          "haut_tronc",
                          "tronc_diam",
                          "fk_stadedev",
-                         "fk_prec_estim"]
+                         "fk_prec_estim",
+                         "fk_revetement"
+                     ]
                      )
-    Y= pd.DataFrame(data,
+    y= pd.DataFrame(data,
                     columns=[
                         "age_estim"]
                     )
 
-    return X, Y
+    return X, y
+
+
 
 
 if __name__ == '__main__':
     d = load_data("Data_Arbre.csv")
-    X, Y = generate_model_age(d)
-    X_train, X_test, Y_train, Y_test = X[:6000], X[6000:], Y[:6000], Y[6000:]
-    clf = RandomForestClassifier(n_estimators=10)
-    clf = clf.fit(X_train, Y_train)
-    X_pred = clf.predict(X_train)
-    print(accuracy_score(Y_train, X_pred))
     """
-    X_train, X_test, Y_train, Y_test = X[:6000], X[6000:], Y[:6000], Y[6000:]
-    SGD = SGDClassifier()
+    X, y = generate_model_age(d)
+    X_train, X_test, y_train, y_test = X[:6000], X[6000:], y[:6000], y[6000:]
+    """
+    create_classes(d)
+    X = d.drop(columns=["age_estim", "age_class"])
+    y = d["age_class"]
 
-    SGD.fit(X_train, Y_train)
+    # Diviser les données en ensembles d'entraînement et de test
+    X_train, X_test, y_train, y_test = X[:6000], X[6000:], y[:6000], y[6000:]
+    clf = RandomForestClassifier(n_estimators=10)
+    clf = clf.fit(X_train, y_train)
+    X_pred = clf.predict(X_test)
+    print("Taux de classification : ",accuracy_score(y_test, X_pred))
+    print("Précision (Precision), Rappel (Recall) : \n",classification_report(y_test, X_pred))
+    print("Matrice de confusion : \n",confusion_matrix(y_test, X_pred))
 
-    print(SGD.predict([X.values[0]]))
-
-    decision_scores = SGD.decision_function([X.values[0]])
-    print(decision_scores)
-
-    #accuracy_scores = model_selection.cross_val_score(SGD, X_train, Y_train, cv=3, scoring='accuracy')
-    accuracy_scores = accuracy_score(Y_test, SGD.predict(X_test))
-    print(accuracy_scores)
-
-
-"""
 
     pass
