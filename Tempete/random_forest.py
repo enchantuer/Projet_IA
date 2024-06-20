52105abfc54d68e5a058abbc870e5cab2cf1d2b1
pbroc <paul.brochoire@isen-ouest.yncrea.fr>
2024-06-20 11:46:40 +0200

    suppression truc inutile

--- Tempete/random_forest.py
+++ Tempete/random_forest.py
@@ -36,25 +36,7 @@ def create_classes(data):
     return data
 
 
-"""def print_graph(result_grid, param_grid,  param_grid1):
-    results = result_grid.cv_results_
-    scores_mean = results['mean_test_score']
-    scores_std = results['std_test_score']
-    params = results['params']
-    scores_mean = scores_mean.reshape(len(param_grid[param_grid1[0]]), len(param_grid[param_grid1[1]]))
-    scores_std = scores_std.reshape(len(param_grid[param_grid1[0]]), len(param_grid[param_grid1[1]]))
-    plt.figure(figsize=(8, 6))
-    for i, value in enumerate(param_grid[param_grid1[0]]):
-        plt.plot(param_grid[param_grid1[1]], scores_mean[i], label=f'param_grid1[0]: {value}')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.xlabel(param_grid1[1])
-    plt.ylabel(param_grid1[0])
-    plt.title('Grid Search Accuracy Results')
-    plt.legend(loc='best')
-    plt.grid(True)
-    plt.show()
-"""
+
 
 if __name__ == '__main__':
     d = load_data("../Data_Arbre.csv")
